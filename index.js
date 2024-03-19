ff65a1d3ee0c139b3a8877c11db4d91f08751b99
neema <neemabridgetteodire2@gmail.com>
2024-03-19 18:15:42 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 100)+1
+let num3 = 24
+let num4 = 6
+let mod = num3 / num4
+let max = Math.max (16,17,18,19,20)
+
+
